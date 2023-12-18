002bbe66f982c6a9b90479ce8a83ae46cd101abb
Keval <77504805+ke-va-l@users.noreply.github.com>
2023-12-18 11:10:13 +0530

    Add files via upload

---
+++ Flutter/Dart programming/First.dart
@@ -0,0 +1,11 @@
+void main() {
+  var name = 'keval';
+  var B_date = '11/12/2003';
+  var age = "20";
+  var address = 'Amreli';
+
+  print(name);
+  print(B_date);
+  print(age);
+  print(address);
+}
